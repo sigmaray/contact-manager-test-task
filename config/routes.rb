8ce36c7114608feb074b334902b90cc767162e71
Stanislav Zaleski <sigmaray@gmail.com>
2020-05-14 09:05:44 +0300

    Setup webpack

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,8 @@ Rails.application.routes.draw do
   end
 
   ActiveAdmin.routes(self)
+
+  root to: 'application#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
